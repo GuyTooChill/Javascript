0e1a4511c014e444d6456b5d03f6c87397206710
Diego G <Diego93gonzo@gmail.com>
2023-06-06 12:13:54 -0400

    updates to ninja

--- Week_1/ninja.js
+++ Week_1/ninja.js
@@ -9,9 +9,17 @@ class Ninja {
         console.log(this.name)
     }
     showStats() {
-        console.log(this.name, this.strength, this.speed, this.health)
+        console.log(this.name, this.health, this.speed, this.strength)
     }
     drinkShake() {
         this.health += 10;
     }
 }
+
+let one = new Ninja("Diego", 100, 5, 10)
+let two = new Ninja("Test")
+
+one.sayName()
+one.showStats()
+two.sayName()
+two.showStats()
