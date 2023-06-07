6253a595ce1db06b619e79bc01998a9bef40c87d
Diego G <Diego93gonzo@gmail.com>
2023-06-07 13:42:47 -0400

    first JSX

---
+++ Week_1/first_JSX/your-project-name-here/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things i need to do:</h2>
+      <ul>
+        Learn React
+      </ul>
+      <ul>
+        Climb Mt. Everest
+      </ul>
+      <ul>
+        Run a Marathon
+      </ul>
+      <ul>
+        Feed the dogs
+      </ul>
+    </div>
+  );
+}
+
+export default App;
