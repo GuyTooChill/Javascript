55fbeb3921ef234a278cc52936e13ef09d8d532f
Diego G <Diego93gonzo@gmail.com>
2023-06-08 12:59:57 -0400

    form update

--- Week_1/hook_form/src/components/UserForm.jsx
+++ Week_1/hook_form/src/components/UserForm.jsx
@@ -5,34 +5,35 @@ const UserForm = () => {
     const [lastname, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
-    const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
+    // const [hasBeenSubmitted, setHasBeenSubmitted] = useState(false);
     
+
     const createUser = (e) => {
         // we must prevent the default refresh of the browser to keep our state from being reset
         e.preventDefault();
-    
+        
         const newUser = {firstname, lastname, email, password};
         console.log("Welcome", newUser);
-        setFirstName("")
-        setLastName("")
-        setEmail("")
-        setPassword("")
-        setHasBeenSubmitted(true);
+        // setFirstName("")
+        // setLastName("")
+        // setEmail("")
+        // setPassword("")
+        // setHasBeenSubmitted(true);
     };
 
-    const formMessage = () => {
-        if( hasBeenSubmitted ) {
-        return "Thank you for submitting the form!";
-    } else {
-        return "Welcome, please submit the form";
-    }
-    };
+    // const formMessage = () => {
+    //     if( hasBeenSubmitted ) {
+    //     return "Thank you for submitting the form!";
+    // } else {
+    //     return "Welcome, please submit the form";
+    // }
+    // };
 
     
     return(
         <div>
         <form onSubmit={ createUser }>
-            <h3>{ formMessage() }</h3>
+            {/* <h3>{ formMessage() }</h3> */}
             <div>
                 <label>First Name: </label> 
                 <input type="text" value={firstname} onChange={ (e) => setFirstName(e.target.value) } />
@@ -55,6 +56,7 @@ const UserForm = () => {
             </div>
             <input type="submit" value="Create User" />
         </form>
+        <hr/>
         <div>
                 <h3>Your Form Data</h3>
                 <p>First Name : {firstname}</p>
