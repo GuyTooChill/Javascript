04d39ff7ad84f618f30b1c9707ae954384c289ec
Diego G <Diego93gonzo@gmail.com>
2023-06-13 20:43:06 -0400

    axios api

--- Week_2/pokemon_api/src/App.js
+++ Week_2/pokemon_api/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import FetchAxiosPokeAPI from './components/AxiosPokemon';
 import FetchPokeAPI from './components/Pokemon';
 
 function App() {
   return (
     <div className="App">
-      <FetchPokeAPI/>
+      {/* <FetchPokeAPI/> */}
+      <FetchAxiosPokeAPI/>
     </div>
   );
 }
