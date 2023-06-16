f681a0da409ce6b0bb893edddf9bb5b3e9d59cc9
Diego G <Diego93gonzo@gmail.com>
2023-06-16 18:32:10 -0400

    faker api updates

--- Week_2/faker_api/server.js
+++ Week_2/faker_api/server.js
@@ -1,10 +1,60 @@
 const express = require("express")
-// const { faker } = require(`@faker-js/faker`)
-
 const app = express()
-
 const port = 8000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.listen(port, () => console.log(`Welcome tarnished`) )
+
+
+const { faker } = require('@faker-js/faker');
+
+const createProduct = () => {
+    const newFake = {
+        name: faker.commerce.productName(),
+        price: "$" + faker.commerce.price(),
+        department: faker.commerce.department()
+    };
+    return newFake;
+};
+    
+const newFakeProduct = createProduct();
+console.log(newFakeProduct);
+
+
+
+const createUser = () => {
+    const newUser = {
+        id: faker.string.uuid(),
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number(),
+        password: faker.internet.password()
+    };
+    return newUser;
+}
+
+const createNewUser = createUser();
+console.log(createNewUser)
+
+
+
+const createObject = () => {
+    const newObject = {
+        id: faker.string.uuid(),
+        name: faker.person.fullName(),
+        address: {
+            street: faker.location.street(),
+            city: faker.location.city(),
+            state: faker.location.state(),
+            zipCode: faker.location.zipCode(),
+            country: faker.location.country()
+        }
+    };
+    return newObject;
+}
+
+const createCompanyObject = createObject();
+console.log(createCompanyObject)
+
